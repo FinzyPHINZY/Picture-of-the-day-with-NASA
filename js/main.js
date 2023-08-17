ad8ab2a7f5ca7477dbf3b123bdcc8b879d95ffd5
FinzyPHINZY <finzyphinzy@gmail.com>
2023-08-17 14:52:59 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+
+document.querySelector("input").addEventListener("click", getPicture);
+
+function getPicture() {
+  const date = document.querySelector("input").value;
+  fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=cjeRMtumMvQw0G3liXLvEBVS5OegIENMxh4Mhf4W&date=${date}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      document.querySelector(".date").textContent = data.date;
+      document.querySelector(".description").textContent = data.explanation;
+      document.querySelector("img").src = data.hdurl;
+      document.querySelector("h2").textContent = data.title;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
